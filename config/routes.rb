f0fce6f58f936d8c981ef071762f97dc370388a0
samash <samrat.shiwakoti@gmail.com>
2015-07-18 23:09:55 -0400

    Added the comments feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :comments
+  resources :comments
   get 'pages/info'
 
   resources :ideas
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719015823) do
+ActiveRecord::Schema.define(version: 20150719024900) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
